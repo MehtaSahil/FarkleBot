f7b8669a0979d03e81036ceb546d258844e8a492
Sahil Mehta <sahilmehta17@gmail.com>
2018-05-15 17:04:07 -0400

    deleted files that were previously needed for simulation

--- dice.h
+++
@@ -1,9 +0,0 @@
-struct Die {
-    int value;
-};
-
-/* returns a number between 1 and num_sides (inclusive) as if rolling */
-int roll(struct Die dice, int num_sides) {
-    dice.value = rand() % num_sides;
-    return dice.value;
-}
